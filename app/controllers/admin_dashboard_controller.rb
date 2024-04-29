2a68b3eaa3b7c72a047517392107d59002cdf6c7
April Rieger <april.rieger@yahoo.com>
2024-04-28 21:50:16 -0700

    fixes

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,6 +1,6 @@
 class AdminDashboardController < ApplicationController
   before_action :require_admin
-  # before_action :require_superadmin
+  before_action :require_superadmin
 
   def index
     @users = User.all
@@ -14,12 +14,6 @@ class AdminDashboardController < ApplicationController
       redirect_to root_path, alert: "You are not authorized to access this page."
     end
   end
-
-  # def require_superadmin
-  #   unless current_user.superadmin?
-  #     redirect_to root_path, alert: "You are not authorized to access this page."
-  #   end
-  # end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
